a4ab462ffb44d993010529647801bd92596899b8
Jamal Jones <90221057+jamalJones2021@users.noreply.github.com>
2025-03-22 20:35:59 +0000

    updated changes

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 
   const data: ImageData[] = [
 
-    {id:1,src:'src/assets/hungry.jpg'},{id:2,src:'src/assets/Paint.jpg'},{id:3,src:'src/assets/readbook.jpg'},{id:4,src:'src/assets/toilet.jpg'}
+    {id:1,src:'/src/assets/hungry.jpg'},{id:2,src:'/src/assets/Paint.jpg'},{id:3,src:'/src/assets/readbook.jpg'},{id:4,src:'/src/assets/toilet.jpg'}
 ]
 
   const clickHandler = (id:number)=>{
