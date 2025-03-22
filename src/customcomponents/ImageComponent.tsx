102eed286bbb48ebcbebdfcc9c5a5bc8ffd53e83
Jamal Jones <90221057+jamalJones2021@users.noreply.github.com>
2025-03-22 20:32:25 +0000

    updated changes

--- src/customcomponents/ImageComponent.tsx
+++ src/customcomponents/ImageComponent.tsx
@@ -1,9 +1,15 @@
-import {useState} from 'react'
 
-export const ImageComponent = ({src, id, handler})=>{ 
+type ImageProps = {
+  id: number,
+  src: string,
+  handler: (id:number) => void
+
+}
+
+export const ImageComponent :React.FC<ImageProps> = ({src, id, handler})=>{ 
   return (
     <div className='flex justify-center items-center'>
-            <img src={src} width="200px" height="200px" id={id} onClick={()=>handler(id)}/>
+            <img src={src} width="200px" height="200px" onClick={()=>handler(id)}/>
     </div>
   )
 }
